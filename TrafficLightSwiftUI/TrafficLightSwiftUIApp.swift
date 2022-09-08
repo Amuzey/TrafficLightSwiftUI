57e1d546234af38003f0d6026cedf1d734d15482
Amuzey <ivanovad97@gmail.com>
2022-09-08 12:59:40 +0500

    Initial Commit

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Алексей on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
