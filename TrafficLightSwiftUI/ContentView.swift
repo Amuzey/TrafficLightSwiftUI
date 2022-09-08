45081d91c0832e9a5dbf9926c709d325a217b6e5
Amuzey <ivanovad97@gmail.com>
2022-09-08 14:12:58 +0500

    create UI

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tapCount = 0
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                Circle()
+                    .foregroundColor(.red)
+                    .frame(width: 130, height: 130)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                Circle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 130, height: 130)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                Circle()
+                    .foregroundColor(.green)
+                    .frame(width: 130, height: 130)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                Spacer()
+                Button(action: { tapCount += 1 }) {
+                    Text("START: \(tapCount)")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(width: 180, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(15)
+                }
+            }
             .padding()
+        }
+    }
+    
+    private func startButtonTap() {
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
